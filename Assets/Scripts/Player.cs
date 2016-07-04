8559b6aa113ca9b1e33dcdf0d5e113a8e52c1ef9
Harp1e <github@bplayer.waitrose.com>
2016-07-04 17:21:27 +0100

    pre-procedural terrain generation stage - working config

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+	public Text gazeText;
+	public float jumpAngleInDegrees;
+	public float jumpSpeed = 10.0f;
+
+	private CardboardHead head;
+	private Rigidbody rb;
+	private bool onFloor;
+
+	// Use this for initialization
+	void Start () {
+	Cardboard.SDK.OnTrigger += PullTrigger;
+	head = GameObject.FindObjectOfType<CardboardHead>();
+	rb = GetComponent<Rigidbody>();
+	}
+
+	void PullTrigger () {
+		if (onFloor) {
+			float jumpAngleInRadians = jumpAngleInDegrees * Mathf.Deg2Rad;
+			Vector3 projectedVector = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
+			Vector3 jumpVector = Vector3.RotateTowards(projectedVector, Vector3.up, jumpAngleInRadians, 0);
+			rb.velocity = jumpVector * jumpSpeed;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+		onFloor = true;
+	}
+
+	void OnCollisionExit(Collision collision){
+		onFloor = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		gazeText.text = head.Gaze.ToString();
+	}
+}
