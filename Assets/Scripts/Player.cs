07e96c8fcdeca633ea0a706f7500d6f8d0b6740e
Harp1e <github@bplayer.waitrose.com>
2016-07-04 22:05:06 +0100

    Following Fire wall added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,13 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
-	public Text gazeText;
 	public float jumpAngleInDegrees;
 	public float jumpSpeed = 10.0f;
 
 	private CardboardHead head;
 	private Rigidbody rb;
 	private bool onFloor;
+	private float lastJumpRequestTime = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -20,24 +20,31 @@ public class Player : MonoBehaviour {
 	}
 
 	void PullTrigger () {
-		if (onFloor) {
-			float jumpAngleInRadians = jumpAngleInDegrees * Mathf.Deg2Rad;
-			Vector3 projectedVector = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
-			Vector3 jumpVector = Vector3.RotateTowards(projectedVector, Vector3.up, jumpAngleInRadians, 0);
-			rb.velocity = jumpVector * jumpSpeed;
+		RequestJump();
+	}
+
+	void RequestJump(){
+		lastJumpRequestTime = Time.time;
+		rb.WakeUp();
+	}
+
+	void Jump(){
+		float jumpAngleInRadians = jumpAngleInDegrees * Mathf.Deg2Rad;
+		Vector3 projectedVector = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
+		Vector3 jumpVector = Vector3.RotateTowards(projectedVector, Vector3.up, jumpAngleInRadians, 0);
+		rb.velocity = jumpVector * jumpSpeed;
+	}
+
+	void OnCollisionStay(Collision collision){
+		float delta = Time.time - lastJumpRequestTime;
+		if (delta < 0.1f){
+			Jump();
+			lastJumpRequestTime = 0;
 		}
-	}
-
-	void OnCollisionEnter(Collision collision){
-		onFloor = true;
-	}
-
-	void OnCollisionExit(Collision collision){
-		onFloor = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		gazeText.text = head.Gaze.ToString();
+
 	}
 }
